102614743f566ff98f8a55b9f1d1f5ae47e54996
TimothySimth <smith.t7@stu.janesville.k12.wi.us>
2025-09-05 10:32:17 -0500

    9/5

---
+++ src/Q1/DataFiles2.java
@@ -0,0 +1,9 @@
+package Q1;
+import java.util.*;
+import java.io.*;
+
+public class DataFiles2 {
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(new File("prog285b.txt"));
+    }
+}
---
+++ src/Q1/prog335e.java
@@ -0,0 +1,11 @@
+package Q1;
+
+public class prog335e {
+    public static void main(String[] args) {
+
+    }
+    public int findNum(int[] arr, int num){
+        return num;
+    }
+
+}
