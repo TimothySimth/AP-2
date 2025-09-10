dc2b1ea26f12e7f31f7b360e41b5e99f104e07c1
TimothySimth <143823817+TimothySimth@users.noreply.github.com>
2025-09-10 15:14:37 -0500

    Update prog410t.java

--- src/Q1/prog410t/prog410t.java
+++ src/Q1/prog410t/prog410t.java
@@ -6,13 +6,22 @@ import java.io.*;
 public class prog410t {
     public static void main(String[] args) throws IOException {
         ArrayList<household> houses = new ArrayList<>();
-        Scanner input = new Scanner(new File("prog285b.txt"));
+        Scanner input = new Scanner(new File("survey.dat"));
         while (input.hasNext()){
             int id = input.nextInt();
             int income = input.nextInt();
             int members = input.nextInt();
             household house = new household(id, income, members);
             houses.add(house);
+        }
+        ArrayList<household> houses2 = new ArrayList<>();
+        input = new Scanner(new File("survey2.dat"));
+        while (input.hasNext()){
+            int id = input.nextInt();
+            int income = input.nextInt();
+            int members = input.nextInt();
+            household house = new household(id, income, members);
+            houses2.add(house);
         }
     }
 }
