9f829b8dc701fbb7cba8915ea65be881f218cc1f
TimothySimth <smith.t7@stu.janesville.k12.wi.us>
2025-09-17 10:48:23 -0500

    9/17

--- src/Q1/prog492h.java
+++ src/Q1/prog492h.java
@@ -20,70 +20,87 @@ public class prog492h {
             }
             System.out.println();
         }
-
-        String[][] mat2 = new String[30][30];
-
-        for (r = 0; r < 30; r++) {
-            int cnt=0;
-            for (int c = 0; c < 30; c++) {
-                //1
-                if (r>=1) {
-                    String ch = mat[r-1][c];
-                    if (ch.equals("*")) {
-                        cnt++;
+        for (int i = 0; i < 15; ++i) {
+            String[][] mat2 = new String[30][30];
+            for (r = 0; r < mat.length; r++) {
+                for (int c = 0; c < mat[r].length; c++) {
+                    int cnt = 0;
+                    //1
+                    if (r >= 1) {
+                        String ch = mat[r - 1][c];
+                        if (ch.equals("*")) {
+                            cnt++;
+                        }
                     }
-                }
-                //2
-                if (r >= 1 && c <= mat[0].length-2) {
-                    String ch = mat[r-1][c+1];
-                    if (ch.equals("*")) {
-                        cnt++;
+                    //2
+                    if (r >= 1 && c <= mat[0].length - 2) {
+                        String ch = mat[r - 1][c + 1];
+                        if (ch.equals("*")) {
+                            cnt++;
+                        }
                     }
-                }
-                //3
-                if ( c <= mat[0].length-2) {
-                    String ch = mat[r][c+1];
-                    if (ch.equals("*")) {
-                        cnt++;
+                    //3
+                    if (c <= mat[0].length - 2) {
+                        String ch = mat[r][c + 1];
+                        if (ch.equals("*")) {
+                            cnt++;
+                        }
                     }
-                }
-                //4
-                if (r <= mat.length-2 && c <= mat[0].length-2) {
-                    String ch = mat[r+1][c+1];
-                    if (ch.equals("*")) {
-                        cnt++;
+                    //4
+                    if (r <= mat.length - 2 && c <= mat[0].length - 2) {
+                        String ch = mat[r + 1][c + 1];
+                        if (ch.equals("*")) {
+                            cnt++;
+                        }
                     }
-                }
-                //5
-                if (r <= mat.length-2) {
-                    String ch = mat[r+1][c];
-                    if (ch.equals("*")) {
-                        cnt++;
+                    //5
+                    if (r <= mat.length - 2) {
+                        String ch = mat[r + 1][c];
+                        if (ch.equals("*")) {
+                            cnt++;
+                        }
                     }
-                }
-                //6
-                if (r <= 2 && c <= mat[0].length-2) {
-                    String ch = mat[r+1][c-1];
-                    if (ch.equals("*")) {
-                        cnt++;
+                    //6
+                    if (r <= mat.length-2 && c >= 1) {
+                        String ch = mat[r + 1][c - 1];
+                        if (ch.equals("*")) {
+                            cnt++;
+                        }
                     }
-                }
-                //7
-                if (r >= 2) {
-                    String ch = mat[r][c-1];
-                    if (ch.equals("*")) {
-                        cnt++;
+                    //7
+                    if (c >= 1) {
+                        String ch = mat[r][c-1];
+                        if (ch.equals("*")) {
+                            cnt++;
+                        }
                     }
-                }
-                //8
-                if (r >= 2 && c >= 2) {
-                    String ch = mat[r-1][c-1];
-                    if (ch.equals("*")) {
-                        cnt++;
+                    //8
+                    if (r >= 1 && c >= 1) {
+                        String ch = mat[r - 1][c - 1];
+                        if (ch.equals("*")) {
+                            cnt++;
+                        }
+                    }
+                    if (cnt <= 1 || cnt >= 4) {
+                        mat2[r][c] = ".";
+                    }
+                    if (cnt == 3) {
+                        mat2[r][c] = "*";
+                    }
+                    if (cnt == 2) {
+                        mat2[r][c] = mat[r][c];
                     }
                 }
             }
-        }
+            mat = mat2;
+            System.out.println("\n");
 
+            for (String[] row : mat) {
+                for (String col : row) {
+                    System.out.print(col + " ");
+                }
+                System.out.println();
+            }
+        }
     }
 }
