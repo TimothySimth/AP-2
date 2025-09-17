4acaf192ab42016b75fe1e15e53cadccf8fef6aa
TimothySimth <143823817+TimothySimth@users.noreply.github.com>
2025-09-17 14:30:58 +0000

    9/17

---
+++ src/Q1/prog470t.java
@@ -0,0 +1,23 @@
+package Q1;
+import java.util.*;
+import java.io.*;
+
+public class prog470t {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(new File("langdat/prg470t1.dat"));
+
+        int[][] mat = new int[4][4];
+        for (int r = 0; r < 4; ++r){
+            for (int c = 0; c < 4; c++){
+                int thing = sc.nextInt();
+                mat[r][c] = thing;
+            }
+        }
+        for (int[] row : mat) {
+            for (int col : row) {
+                System.out.print(col+" ");
+            }
+            System.out.println();
+        }
+    }
+}
